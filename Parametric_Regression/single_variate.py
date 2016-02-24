2c0a04b2ead219f33d75e30d8278799693c4931b
arajago6 <arajago6@hawk.iit.edu>
2016-02-24 02:23:40 -0600

    Finish cross validation routine

--- Parametric_Regression/single_variate.py
+++ Parametric_Regression/single_variate.py
@@ -38,3 +38,39 @@ def crossValidate(attributes, outcomes, foldCount, degree, ownFunction = True):
 
 	testDataList = copy.copy(zMatrixFolds)
 	testOtcmList = copy.copy(otcmFolds)
+
+	for itr in range(foldCount):
+		trainDataList = []
+		trainOtcmList = []
+		for intitr in range (foldCount):
+			if intitr != itr:
+				trainDataList.append(zMatrixFolds[intitr]) 
+				trainOtcmList.append(otcmFolds[intitr])
+
+		trainDataArr = 	np.array(trainDataList).reshape(-1,degree+1)
+		trainOtcmArr =  np.array(trainOtcmList).reshape(-1)
+		testDataArr = np.array(testDataList[itr])
+		testOtcmArr = np.array(testOtcmList[itr])
+		
+		if ownFunction:
+			if degree == 1:
+				thetaVal = solveLinearModel(trainDataArr,trainOtcmArr)	
+			else:	
+				thetaVal = solvePolyModel(trainDataArr,trainOtcmArr)
+			trainingEstimate = estimate(thetaVal,trainDataArr) 
+			testingEstimate = estimate(thetaVal,testDataArr)			
+		else:			
+			lR = LinearRegression()
+			lR.fit(trainDataArr,trainOtcmArr)
+			thetaVal = lR.coef_
+			trainingEstimate = lR.predict(trainDataArr) 
+			testingEstimate = lR.predict(testDataArr)
+
+		trainingError = meanSqrError(trainingEstimate, trainOtcmArr)
+		testingError = meanSqrError(testingEstimate,testOtcmArr)
+	
+		thetaValList.append(thetaVal)
+        	trainingErrorList.append(trainingError)
+        	testingErrorList.append(testingError)
+	
+	return zMatrix, thetaValList, trainingErrorList, testingErrorList
