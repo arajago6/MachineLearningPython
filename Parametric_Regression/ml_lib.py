f59d5b2c20f33a1a764324b8e02145262d6c51ed
arajago6 <arajago6@hawk.iit.edu>
2016-02-25 12:26:55 -0600

    Add function for drawing plot

--- Parametric_Regression/ml_lib.py
+++ Parametric_Regression/ml_lib.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def drawPlot(inptData,plotTitle):
+	plt.scatter(inptData[0], inptData[1], color='black')
+	plt.title(plotTitle)
+	if len(inptData)>2:
+		plt.scatter(inptData[2], inptData[3], color='r')
+	plt.show()
