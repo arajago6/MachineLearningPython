dc57ee86c40e21d905fee2f231f3df46ac863354
arajago6 <arajago6@hawk.iit.edu>
2016-02-27 23:59:31 -0600

    Start cross validation and finish solve

--- Parametric_Regression/multi_variate.py
+++ Parametric_Regression/multi_variate.py
@@ -7,3 +7,20 @@ from scipy.spatial.distance import cdist
 # Map data to higher dimensional space
 def mapHD(inptData,degree):
     return PolynomialFeatures(degree).fit_transform(inptData)
+
+
+def solve(inptData,outcomes):
+    return np.dot(np.linalg.pinv(inptData),outcomes)
+
+
+def crossValidate(zMatrix, outcomes, foldCount):
+	thetaValList = []
+    	trainingErrorList =[]
+    	testingErrorList = []
+	featSize = zMatrix.shape[1]
+
+	zMatrixFolds = np.asarray(getFolds(zMatrix,foldCount))
+	otcmFolds = np.asarray(getFolds(outcomes,foldCount))
+
+	testDataList = copy.copy(zMatrixFolds)
+	testOtcmList = copy.copy(otcmFolds)
