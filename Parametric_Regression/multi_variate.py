98b7a9d35bc899e7e58f888b371ea1c029f09f00
arajago6 <arajago6@hawk.iit.edu>
2016-03-02 23:57:44 -0600

    Finish cross validation

--- Parametric_Regression/multi_variate.py
+++ Parametric_Regression/multi_variate.py
@@ -24,3 +24,30 @@ def crossValidate(zMatrix, outcomes, foldCount):
 
 	testDataList = copy.copy(zMatrixFolds)
 	testOtcmList = copy.copy(otcmFolds)
+
+	for itr in range(foldCount):
+		trainDataList = []
+		trainOtcmList = []
+		for intitr in range (foldCount):
+			if intitr != itr:
+				trainDataList.append(zMatrixFolds[intitr]) 
+				trainOtcmList.append(otcmFolds[intitr])
+
+		trainDataArr = 	np.array(trainDataList).reshape(-1,featSize)
+		trainOtcmArr =  np.array(trainOtcmList).reshape(-1,1)
+		testDataArr = np.array(testDataList[itr])
+		testOtcmArr = np.array(testOtcmList[itr])
+
+		thetaVal  = solve(trainDataArr,trainOtcmArr)
+		trainingEstimate = estimate(thetaVal,trainDataArr) 
+		testingEstimate = estimate(thetaVal,testDataArr)
+
+		trainingError = meanSqrError(trainingEstimate, trainOtcmArr)
+		testingError = meanSqrError(testingEstimate,testOtcmArr)
+	
+		thetaValList.append(thetaVal)
+        	trainingErrorList.append(trainingError)
+        	testingErrorList.append(testingError)
+	
+	return zMatrix, thetaValList, trainingErrorList, testingErrorList
+
