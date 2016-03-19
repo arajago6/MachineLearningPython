d3864f88a5f0b83efd8df3e52a08aea2dd30a3f5
arajago6 <arajago6@hawk.iit.edu>
2016-03-18 23:50:32 -0500

    Add routines to get specific mean and variances

--- Generative_Learning/code/1_gda.py
+++ Generative_Learning/code/1_gda.py
@@ -3,3 +3,13 @@ import pandas as pd
 import copy
 from ml_lib import *
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+def getSpecificMean(attributes, outcomes, dsrdOutcome, colNum=0):
+	dsrdOList = ([attributes[i] for i in range(len(outcomes)) if outcomes[i]==dsrdOutcome])
+	return np.mean(np.array(dsrdOList)[:,colNum])
+
+
+def getSpecificVar(attributes, outcomes, dsrdOutcome, colNum=0):
+	dsrdOList = ([attributes[i] for i in range(len(outcomes)) if outcomes[i]==dsrdOutcome])
+	return np.std(np.array(dsrdOList)[:,colNum])
