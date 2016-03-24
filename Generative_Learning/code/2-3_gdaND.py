19f90079e39bf0e6ccb86edd85663464ddaa4826
arajago6 <arajago6@hawk.iit.edu>
2016-03-23 23:09:13 -0500

    Add routines to get params and membership

--- Generative_Learning/code/2-3_gdaND.py
+++ Generative_Learning/code/2-3_gdaND.py
@@ -4,3 +4,18 @@ from ml_lib import *
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 import warnings
 warnings.filterwarnings("ignore")
+
+
+# This function calculates the membership
+def gdaNDMemberFn(x,sMean,sSig,pClassProb):
+	return(-np.log(np.linalg.det(sSig))- 0.5*(np.dot(np.dot(np.transpose(x-sMean),np.linalg.inv(sSig)),(x-sMean))) + np.log(pClassProb))
+
+
+# This function gets parameters of the feature distribution
+def getParams(trainDataArr,trainOtcmArr,otcmVal,featLen):
+	muVal = {}; prior = {}; sig = {}; 
+	for each in otcmVal:
+		muVal[each] = np.array([getSpecificMean(trainDataArr,trainOtcmArr,each,j) for j in range(featLen)])
+		prior[each] = [len([trainOtcmArr[i] for i in range(len(trainOtcmArr)) if trainOtcmArr[i]==each])/float(len(trainOtcmArr))]
+		sig[each] = np.array(np.cov([trainDataArr[i] for i in range(len(trainOtcmArr)) if trainOtcmArr[i]==each],rowvar=0))
+	return dict({"muVal":muVal,"prior":prior,"sig":sig})
