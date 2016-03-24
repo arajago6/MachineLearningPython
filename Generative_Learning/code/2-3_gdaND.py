326c7087624fd0f6ff40e2e9f9cca4bbb7bced2e
arajago6 <arajago6@hawk.iit.edu>
2016-03-23 23:06:15 -0500

    Start ND Gaussian Discriminant Analysis

---
+++ Generative_Learning/code/2-3_gdaND.py
@@ -0,0 +1,6 @@
+# Importing the necessary packages
+import numpy as np, copy, operator
+from ml_lib import *
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+import warnings
+warnings.filterwarnings("ignore")
