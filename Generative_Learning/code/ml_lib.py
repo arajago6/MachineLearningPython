5efd5054d2a0f5deb4da935388f6d36694869a30
arajago6 <arajago6@hawk.iit.edu>
2016-03-29 08:47:53 -0500

    Add routine to get variance for a column

--- Generative_Learning/code/ml_lib.py
+++ Generative_Learning/code/ml_lib.py
@@ -7,3 +7,9 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_c
 def getSpecificMean(attributes, outcomes, dsrdOutcome, colNum=0):
 	dsrdOList = ([attributes[i] for i in range(len(outcomes)) if outcomes[i]==dsrdOutcome])
 	return np.mean(np.array(dsrdOList)[:,colNum])
+
+
+# This function gets variance of a specific column for the records of given class
+def getSpecificVar(attributes, outcomes, dsrdOutcome, colNum=0):
+	dsrdOList = ([attributes[i] for i in range(len(outcomes)) if outcomes[i]==dsrdOutcome])
+	return np.std(np.array(dsrdOList)[:,colNum])
