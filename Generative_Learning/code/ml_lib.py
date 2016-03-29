4c5332aa7abbf9c0a83fffdfb56580066539f023
arajago6 <arajago6@hawk.iit.edu>
2016-03-28 23:47:24 -0500

    Add routine to get mean for a column

--- Generative_Learning/code/ml_lib.py
+++ Generative_Learning/code/ml_lib.py
@@ -1,3 +1,9 @@
 # Importing the necessary packages
 import numpy as np, pylab as pl
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_curve, auc, f1_score, precision_recall_curve
+
+
+# This function gets mean of a specific column for the records of given class
+def getSpecificMean(attributes, outcomes, dsrdOutcome, colNum=0):
+	dsrdOList = ([attributes[i] for i in range(len(outcomes)) if outcomes[i]==dsrdOutcome])
+	return np.mean(np.array(dsrdOList)[:,colNum])
