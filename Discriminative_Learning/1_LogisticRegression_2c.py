53669c688c5addb88e208698f4fbc9148483c67f
arajago6 <arajago6@hawk.iit.edu>
2016-04-14 01:46:24 -0500

    Start discriminative learning

---
+++ Discriminative_Learning/1_LogisticRegression_2c.py
@@ -0,0 +1,9 @@
+# Import the necessary packages
+from ml_lib import *
+import numpy as np
+import copy
+from sklearn import preprocessing
+from random import randrange
+from sklearn.linear_model import LogisticRegression
+import warnings
+warnings.filterwarnings("ignore")
