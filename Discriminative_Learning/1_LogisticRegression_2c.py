007a33384c65968c1da1c22620c1b967e60acc7b
arajago6 <arajago6@hawk.iit.edu>
2016-04-14 01:47:17 -0500

    Add routine for gradient descent

--- Discriminative_Learning/1_LogisticRegression_2c.py
+++ Discriminative_Learning/1_LogisticRegression_2c.py
@@ -7,3 +7,19 @@ from random import randrange
 from sklearn.linear_model import LogisticRegression
 import warnings
 warnings.filterwarnings("ignore")
+    
+    
+# To get the final parameters by using iterative parameter update
+def gradDescentFunction(attributes, outcomes, learningRate, iterCountMax, threshold):
+    params = [float(randrange(1,5))/10000 for x in range(attributes.shape[1])]
+    for itr in range(iterCountMax):
+        paramCorrection=[]
+        for intItr in range(attributes.shape[0]):
+            paramCorrection.append((logisticFunction(params,attributes[intItr]) - outcomes[intItr]) * attributes[intItr])
+        updatedParams = params - (learningRate*sum(paramCorrection))
+        llhDiff = logLikelihoodFunction2c(updatedParams,attributes,outcomes) - logLikelihoodFunction2c(params,attributes,outcomes)
+        if llhDiff < threshold:
+			break
+        params = updatedParams
+        
+    return params
