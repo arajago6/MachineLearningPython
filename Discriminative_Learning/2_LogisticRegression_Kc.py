ae1dea412b751022a14694989346cfc692e84bcb
arajago6 <arajago6@hawk.iit.edu>
2016-04-20 03:51:04 -0500

    Finish cross validation and test script

--- Discriminative_Learning/2_LogisticRegression_Kc.py
+++ Discriminative_Learning/2_LogisticRegression_Kc.py
@@ -97,3 +97,78 @@ def crossValidate(attributes, outcomes, foldCount, learningRate, iterCountMax, t
         trainOtcmArr =  np.array(trainOtcmList).reshape(-1)
         testDataArr = np.array(testDataList[itr])
         testOtcmArr = np.array(testOtcmList[itr])
+		
+        if ownFunction:
+            params = logisticRegressionKc(trainDataArr,trainOtcmArr, learningRate, iterCountMax, threshold)
+            testingEstimate = getEstimate(testDataArr,params, otcmVal, testingEstimate)		
+        else:			
+            lR = LogisticRegression()
+            lR.fit(trainDataArr,trainOtcmArr)
+            testingEstimate = lR.predict(testDataArr)
+            
+        metric = getMetrics(testOtcmArr,testingEstimate,otcmVal)
+        accrList.append(metric[0])
+        presList.append(metric[1])
+        recallList.append(metric[2])
+        fMeasList.append(metric[3])
+	
+    return accrList, presList, recallList, fMeasList
+
+
+# This function tests the above functions, when this file is called directly from terminal
+def testScript():
+	print "\n---> Started Logistic Regression - Iris dataset - Own function - k class...\n"
+	attributes, outcomes = getDataFromFile("../Data/iriskc.data.shuffled")
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+	attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+	#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, learningRate=0.01, iterCountMax=750, threshold=0.005, ownFunction=True)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),\
+																												np.mean(recallValues),np.mean(fMeasValues))
+															
+																												
+	print "---> Started Logistic Regression - Iris dataset - Inbuilt function - k class...\n"
+	attributes, outcomes = getDataFromFile("../Data/iriskc.data.shuffled")
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+	attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+	#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, learningRate=0.01, iterCountMax=750, threshold=0.005, ownFunction=False)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),\
+																												np.mean(recallValues),np.mean(fMeasValues))
+
+
+	print "---> Started Logistic Regression - Digits dataset - Own function - k class...\n"																									
+	mnist = datasets.fetch_mldata('MNIST original')
+	X, y = mnist.data / 255., mnist.target
+	attributes = X[:20000]
+	outcomes = y[:20000]
+	#print list(set(outcomes))
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, learningRate=0.01, iterCountMax=100, threshold=0.005, ownFunction=False)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),\
+																												np.mean(recallValues),np.mean(fMeasValues))
+
+
+	print "---> Started Logistic Regression - Digits dataset - Inbuilt function - k class...\n"																									
+	mnist = datasets.fetch_mldata('MNIST original')
+	X, y = mnist.data / 255., mnist.target
+	attributes = X[:20000]
+	outcomes = y[:20000]
+	#print list(set(outcomes))
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, learningRate=0.01, iterCountMax=100, threshold=0.005, ownFunction=False)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),\
+																												np.mean(recallValues),np.mean(fMeasValues))
+	
+	
+if __name__ == "__main__":
+    testScript()
+
