68b13089923fd9f0d0455b927ca4d83dadcf549f
arajago6 <arajago6@hawk.iit.edu>
2016-04-20 03:44:41 -0500

    Finish cross-validation and test script

--- Discriminative_Learning/1_LogisticRegression_2c.py
+++ Discriminative_Learning/1_LogisticRegression_2c.py
@@ -73,3 +73,108 @@ def crossValidate(attributes, outcomes, foldCount, degree, learningRate, iterCou
         trainOtcmArr =  np.array(trainOtcmList).reshape(-1)
         testDataArr = np.array(testDataList[itr])
         testOtcmArr = np.array(testOtcmList[itr])
+		
+        if ownFunction:
+            params = logisticRegressionFunction2c(trainDataArr,trainOtcmArr, learningRate, iterCountMax, threshold)
+            testingEstimate = getEstimate(testDataArr,params)		
+        else:			
+            lR = LogisticRegression()
+            lR.fit(trainDataArr,trainOtcmArr) 
+            testingEstimate = lR.predict(testDataArr)
+            
+        metric = getMetrics(testOtcmArr,testingEstimate,otcmVal)
+        accrList.append(metric[0])
+        presList.append(metric[1])
+        recallList.append(metric[2])
+        fMeasList.append(metric[3])
+	
+    return accrList, presList, recallList, fMeasList
+
+
+# This function tests the above functions, when this file is called directly from terminal
+def testScript():
+	print "\n---> Started Logistic Regression - Iris dataset - Own function - 2 class...\n"
+	attributes, outcomes = getDataFromFile("../Data/iris2c.data.shuffled",classAtStart=False)
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+	attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+	#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, degree=1, learningRate=0.01, \
+																						iterCountMax=750, threshold=0.005, ownFunction=True)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),np.mean(recallValues),np.mean(fMeasValues))
+
+	
+	print "---> Started Logistic Regression - Iris dataset - Inbuilt function - 2 class...\n"
+	attributes, outcomes = getDataFromFile("../Data/iris2c.data.shuffled",classAtStart=False)
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+	attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+	#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, degree=1, learningRate=0.01, \
+																						iterCountMax=750, threshold=0.005, ownFunction=False)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),np.mean(recallValues),np.mean(fMeasValues))
+
+
+	print "---> Started Logistic Regression - Wine dataset - Own Function - 2 class...\n"
+	attributes, outcomes = getDataFromFile("../Data/wine2c.data.shuffled",classAtStart=True)
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+	attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+	#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, degree=1, learningRate=0.01, \
+																						iterCountMax=750, threshold=0.005, ownFunction=True)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),np.mean(recallValues),np.mean(fMeasValues))
+	
+	
+	print "---> Started Logistic Regression - Wine dataset - Inbuilt function - 2 class...\n"
+	attributes, outcomes = getDataFromFile("../Data/wine2c.data.shuffled",classAtStart=True)
+	min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+	attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+	#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+	accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, degree=1, learningRate=0.01, \
+																						iterCountMax=750, threshold=0.005, ownFunction=False)
+	for itr in range(10):
+		print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+	print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),np.mean(recallValues),np.mean(fMeasValues))
+
+
+	for d in range(2,5):
+		print "---> Using non-linear combinations of input to increase capacity - Degree: %s" % d
+		print "Started Logistic Regression - Wine dataset - Own Function - 2 class...\n"
+		attributes, outcomes = getDataFromFile("../Data/wine2c.data.shuffled",classAtStart=True)
+		min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+		attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+		#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+		accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, degree=d, learningRate=0.01, \
+																							iterCountMax=750, threshold=0.005, ownFunction=True)
+		for itr in range(10):
+			print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+		print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),np.mean(recallValues),np.mean(fMeasValues))
+		
+		
+	for d in range(2,5):
+		print "---> Using non-linear combinations of input to increase capacity - Degree: %s" % d
+		print "Started Logistic Regression - Iris dataset - Own Function - 2 class...\n"
+		attributes, outcomes = getDataFromFile("../Data/iris2c.data.shuffled")
+		min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+		attributes, outcomes = min_max_scaler.fit_transform(np.array(attributes)), np.array(outcomes)
+		#attributes, outcomes = np.array(attributes), np.array(outcomes)
+
+		accrValues, presValues, recallValues, fMeasValues = crossValidate(attributes, outcomes, 10, degree=d, learningRate=0.01, \
+																							iterCountMax=750, threshold=0.005, ownFunction=True)
+		for itr in range(10):
+			print "Fold %d: \tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f" %(itr+1,accrValues[itr],presValues[itr],recallValues[itr],fMeasValues[itr])
+		print "\nMean values:\tAccuracy: %f\tPrecision: %f\tRecall: %f\tF-Measure: %f\n" % (np.mean(accrValues),np.mean(presValues),np.mean(recallValues),np.mean(fMeasValues))
+		
+	
+if __name__ == "__main__":
+    testScript()
+
