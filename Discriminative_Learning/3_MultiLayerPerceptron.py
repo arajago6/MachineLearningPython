c700e4507e9425c1269796dccad7317c2c9bd11a
arajago6 <arajago6@hawk.iit.edu>
2016-05-29 19:18:34 -0500

    Add routine to get predictions

--- Discriminative_Learning/3_MultiLayerPerceptron.py
+++ Discriminative_Learning/3_MultiLayerPerceptron.py
@@ -7,3 +7,19 @@ from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
 import warnings
 warnings.filterwarnings("ignore")
+
+
+# Returns predictions with the input test data, class labels, hidden layer parameters, output layer parameters and hidden unit count
+def getEstimate(attributes,olParam,hlParam,otcmVal,huCount,testingEstimate):
+    for i in range(attributes.shape[0]):
+        prevEst, otcmMax = 0, 0
+        currEst = []
+        hlOutput = [logisticFunction(hlParam[j],attributes[i]) for j in range(huCount)]
+
+        for y in range(len(otcmVal)):
+            currEst.append(softmaxFunction(olParam,hlOutput,y,otcmVal))
+            if(currEst[y] > prevEst):
+                otcmMax = y
+                prevEst = currEst[y]
+        testingEstimate.append(otcmVal[otcmMax])
+    return testingEstimate   
