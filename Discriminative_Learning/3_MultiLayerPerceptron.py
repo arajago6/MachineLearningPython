8964331f233cc1e25d7e999a6c9f6b0c6f9832ca
arajago6 <arajago6@hawk.iit.edu>
2016-05-29 19:10:08 -0500

    Start multilayer perceptron

---
+++ Discriminative_Learning/3_MultiLayerPerceptron.py
@@ -0,0 +1,9 @@
+# Import the necessary packages
+import numpy as np
+import random
+from ml_lib import *
+import copy
+from sklearn import preprocessing
+from sklearn.neural_network import MLPClassifier
+import warnings
+warnings.filterwarnings("ignore")
